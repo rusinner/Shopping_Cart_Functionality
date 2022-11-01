4423762a5ffab4b8c6c1bf631980e7708fb38e92
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-01 16:44:54 +0200

    it 's been fixed

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ class UI{
         if(inCart){
             button.innerText = 'In Cart';
             button.disabled = true;
-        }else{
+        }
             button.addEventListener('click', (event)=>{
              event.target.innerText = 'In Cart';
              event.target.disabled = true;
@@ -89,14 +89,14 @@ class UI{
              //show the cart
             this.showCart();
             });
-        }
-    });
+        });
+    
   }
   setCartValues(cart){
     let tempTotal = 0;
     let itemsTotal = 0;
     cart.map(item => {
-     tempTotal + item.price * item.amount;
+     tempTotal += item.price * item.amount;
      itemsTotal += item.amount;
     });
     cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
